89d883406b51200e6f615b670becf4e4b9a7c2ce
yasmeencole <yasmeencole1@mail.com>
2021-05-05 16:44:35 -0500

    added fixture files, addedd views files. able to login aa a registered user

--- levelup/urls.py
+++ levelup/urls.py
@@ -1,21 +1,10 @@
-"""levelup URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from levelupapi.views import register_user, login_user
 
+# the register_user and login_user functions are imported into the module. Then they are used to map a route to that view.
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('register', register_user),
+    path('login', login_user),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
